fde66de85b1715649894cd53bbc92c012c189fd3
Ivan Vegner <vegner@slrealty.ru>
2018-02-13 18:35:19 +0300

    Stub a test

---
+++ tests/ConfigAwareTraitTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+use SD\Config\ConfigAwareTrait;
+use SD\DependencyInjection\Container;
+
+class ConfigAwareTraitTest extends TestCase
+{
+    public function testTrait() {
+        $value = 'yada yada';
+        $config = [
+            'consumer' => $value,
+        ];
+        $container = new Container([
+            'config' => $config,
+        ]);
+        $consumer = $container->produce(Consumer::class);
+        $this->assertEquals($value, $consumer->getConsumerConfig(), 'Must return the provided value');
+    }
+}
